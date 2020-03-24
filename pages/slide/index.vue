2d68b0e12b4a829b9c0ea92e105ad91b66f7f3e5
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2020-03-25 00:57:15 +0900

    add : embed speaker deck slide

---
+++ pages/slide/index.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .slide-top
+    .slide-list
+      .embed-slide(
+          v-for="(embed, index) in embeds"
+          :key="index"
+        )
+        speaker-deck(
+          :id="embed.id"
+        )
+</template>
+
+<script>
+import SpeakerDeck from '../../components/atoms/SpeakerDeck'
+
+export default {
+  components: {
+    SpeakerDeck
+  },
+  data() {
+    return {
+      embeds: [
+        { id: '3458b05bc3774f58b3ef9f38e3441d62' },
+        { id: 'c0d47535f53d4c80ae69ecde8fb4965c' },
+        { id: '29a89b60233c4bfdba640bf24cd0f3ca' },
+        { id: 'a68ef493957841459a1363c2471eed76' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.embed-slide {
+  margin: 3vh 0;
+  width: 100%;
+}
+</style>
